a5fa197b430e813cd3e861cd75d2ef475d9f977a
Brian Ketelsen <mail@bjk.fyi>
2019-10-24 21:00:59 +0000

    initial mac support

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,6 +4,15 @@ cd "$HOME" || return
 system_type=$(uname -s)
 if [[ $system_type == "Darwin" ]]; then
 	echo "> Mac" 
+  # fix that zsh nonsense
+  brew install bash
+  chsh -s /usr/local/bin/bash
+  cd ~/bin/setup/
+
+	./bashmarks.sh
+	./nvm.sh  
+	./rust.sh  
+
 fi
 if [[ $system_type == "Linux" ]]; then 
 	sudo apt-get install -y $(cat ~/.local/share/chezmoi/pkglist.txt | awk '{print $1}')
