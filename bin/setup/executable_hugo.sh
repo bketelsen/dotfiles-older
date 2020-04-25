f9c51c8c4543347faf86882ac599878ef524cbd3
Brian Ketelsen <mail@bjk.fyi>
2020-04-25 15:23:07 +0000

    bump hugo

--- bin/setup/executable_hugo.sh
+++ bin/setup/executable_hugo.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 cd /tmp
-wget https://github.com/gohugoio/hugo/releases/download/v0.57.2/hugo_extended_0.57.2_Linux-64bit.tar.gz
-tar zxfv hugo_extended_0.57.2_Linux-64bit.tar.gz
-sudo mv hugo /usr/local/bin/
-sudo chmod +x /usr/local/bin/hugo
+#wget https://github.com/gohugoio/hugo/releases/download/v0.57.2/hugo_extended_0.57.2_Linux-64bit.tar.gz
+#tar zxfv hugo_extended_0.57.2_Linux-64bit.tar.gz
+#sudo mv hugo /usr/local/bin/
+#sudo chmod +x /usr/local/bin/hugo
+
+wget https://github.com/gohugoio/hugo/releases/download/v0.69.2/hugo_extended_0.69.2_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.69.2_Linux-64bit.deb
+rm hugo_extended_0.69.2_Linux-64bit.deb
+
 
