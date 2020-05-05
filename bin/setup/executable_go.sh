efb38e96974ece9de38b1ce5d83562f94a6cf87b
Brian Ketelsen <mail@bjk.fyi>
2020-05-05 14:17:15 +0000

    update Go, make version a variable

--- bin/setup/executable_go.sh
+++ bin/setup/executable_go.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 set -e
+VERSION=1.14.2
+# Delete existing Go installation
 sudo rm -rf /usr/local/go
-sudo mkdir -p /usr/local/go && curl -Ls https://storage.googleapis.com/golang/go1.14.1.linux-amd64.tar.gz | sudo tar xvzf - -C /usr/local/go --strip-components=1
+# Install Go in /usr/local
+sudo mkdir -p /usr/local/go && curl -Ls https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz | sudo tar xvzf - -C /usr/local/go --strip-components=1
 
