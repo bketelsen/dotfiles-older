37ca89e4cc9683c8a2604d77bdc3d8513b07fafc
Brian Ketelsen <mail@bjk.fyi>
2020-06-16 09:46:12 -0400

    syncthing

---
+++ bin/setup/executable_syncthing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get install syncthing
