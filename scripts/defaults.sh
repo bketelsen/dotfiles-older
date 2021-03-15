9545ddc16013a626bff87523cf17e5839cc4ce1a
Brian Ketelsen <bketelsen@gmail.com>
2021-03-15 09:09:41 -0400

    add the new

---
+++ scripts/defaults.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+defaults write com.apple.dock workspaces-auto-swoosh -bool NO
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write com.apple.screencapture location /Users/bjk/Pictures/Screenshots
+
